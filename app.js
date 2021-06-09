aa69e1e94a52b0759b3f91f70816d92ab7dc2644
prasertcbs <bluecotton.k@gmail.com>
2021-06-09 20:33:15 +0700

    add about.html

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 require('dotenv').config();
 
 const { BAHTTEXT } = require('./bahttext');
@@ -16,7 +17,9 @@ const toWords = new ToWords({
 });
 
 app.get('/', (req, res) => {
-  res.send('Webservice for Excel by @prasertcbs');
+  // res.send('Webservice for Excel by @prasertcbs');
+  // res.sendFile(about.html)
+  res.sendFile(path.join(__dirname, '/about.html'));
 });
 
 app.get('/th/:number', (req, res) => {
